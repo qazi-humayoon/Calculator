43d81227c441a8981b1d6228eaad2a8c6a33e384
HUMAYOON NIYAZ <94228059+qazi-humayoon@users.noreply.github.com>
2024-02-09 22:41:49 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,23 @@
+let string = ""
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach(function (button){
+    button.addEventListener('click',function (e){
+        if(e.target.innerHTML === '='){
+            string = eval(string)
+            document.querySelector('input').value = string;
+        }
+
+        else if(e.target.innerHTML == 'C'){
+            string = ''
+            document.querySelector('input').value = string
+
+        }
+        else{
+
+        string = string + e.target.innerHTML
+        document.querySelector('input').value = string;
+        }
+        
+    })
+})
+
